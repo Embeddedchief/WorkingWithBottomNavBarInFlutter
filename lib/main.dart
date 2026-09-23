cf748f7bbd9808651de7a188b7b5ef236599bcd7
Embeddedchief <samuelsetemijp@gmail.com>
2026-09-23 16:09:26 +0100

    #update

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(bottomNavigationBar: BottomNavigationBar(items: ));
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_outlined),
+            label: "Search",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outlined),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
   }
 }
