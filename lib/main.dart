c84a070173b9e9bdfc26b6266ffbfc961d38d681
Embeddedchief <samuelsetemijp@gmail.com>
2026-09-24 00:13:08 +0100

    #update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:navbar_learning/profile.dart';
+import 'package:navbar_learning/search.dart';
+import 'package:navbar_learning/home.dart';
 
 void main() {
   runApp(NavigationApp());
@@ -9,33 +12,52 @@ class NavigationApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Home> {
+  int _currentIndex = 0;
+
+  List<Widget> pages = [HomePage(), SearchPage(), ProfilePage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      body: pages[_currentIndex],
+      
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
-            label: "Home",
+            label: "Feel at home",
           ),
 
           BottomNavigationBarItem(
             icon: Icon(Icons.search_outlined),
-            label: "Search",
+            label: "Look for things",
           ),
 
           BottomNavigationBarItem(
             icon: Icon(Icons.person_outlined),
-            label: "Profile",
+            label: "Meet me",
           ),
         ],
+
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
